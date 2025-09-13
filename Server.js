f3cd7f992a31dab1bb3fa4fce411340c326722e0
Arnav Sharma <123370153+Arnav270803@users.noreply.github.com>
2025-09-13 14:37:27 +0530

    Checking whether the gitignore is working or not

--- Server.js
+++ Server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
-const app=express();
+import connectDB from './config/mongodb.js';
 
+const app=express();
 const PORT =  4000;
 
 app.use(express.json());
 app.use(cors());
+await connectDB()// connecting the mongodb database
 
 app.listen(PORT , ()=>
 console.log('Server is running on port' + PORT)
