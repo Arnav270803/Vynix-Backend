86e5a4874cc73ce629e95aeaf3f5f87eeb6e8622
Arnav Sharma <123370153+Arnav270803@users.noreply.github.com>
2025-12-22 00:24:08 +0530

    Refactoring in process for video streaming

--- routes/aiRoute.js
+++ routes/aiRoute.js
@@ -12,7 +12,14 @@ airoute.get('/test' , testPrompt)
 
 {/* Streaming video code to the frontend is starting from here  */}
 airoute.get('/video/:uniqueId' , (req,res) => {// ":uniqueId is dynamic in nature and here the real path will be added by express"
-    
+    const uniqueId = req.params.uniqueId
+    const videoPath = path.join(process.cwd() , 'videos' , `anim_${uniqueId}.mp4`); // anim is just a fixed word that is added in front of the file name 
+
+      if (!fs.existsSync(videoPath)) {
+    return res.status(404).json({success: false, message: 'Video not found or has expired', });
+  }
+
+  {/*continue writing from here  */}
 })
 
 export default airoute;
