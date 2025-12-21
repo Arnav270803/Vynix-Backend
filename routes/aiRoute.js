8287477e04785fad68ea053b42e9e8fefdb9949c
Arnav Sharma <123370153+Arnav270803@users.noreply.github.com>
2025-12-21 23:52:19 +0530

    Starting video streaming code :

--- routes/aiRoute.js
+++ routes/aiRoute.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import {sendPrompt , testPrompt} from '../controller/aiController.js'
-
+import path from 'path'
+import fs from 'fs';
 
 const airoute = express.Router();
 
 airoute.post('/ask' , sendPrompt);// Now it is requiting token from the jwt that we have generated 
 airoute.get('/test' , testPrompt)
+
+
+
+{/* Streaming video code to the frontend is starting from here  */}
+airoute.get('/video/:uniqueId' , (req,res) => {// ":uniqueId is dynamic in nature and here the real path will be added by express"
+    
+})
 
 export default airoute;
